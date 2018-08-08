3b4c54025762abc30da98f1f28d6c7e93b75a7d4
Patryk Gułaś <patryk.gulas@digifit.pl>
2018-08-08 14:01:09 +0200

    Add handlebars and improve templates

--- templates/index.js
+++
@@ -1,8 +0,0 @@
-const utils = require('../lib/utils');
-
-getIndexTemplate = (componentName, withContainer) => {
-  return `${!withContainer ? `export { default as ${utils.capitalize(componentName)} } from './${utils.capitalize(componentName)}';\n` : ''}` +
-    `${withContainer ? `export { default as ${utils.capitalize(componentName)} } from './${utils.capitalize(componentName)}.container';\n` : ''}`;
-};
-
-module.exports.getIndexTemplate = getIndexTemplate;
--- templates/styles.js
+++
@@ -1,9 +0,0 @@
-getStylesTemplate = () => {
-  return `import { createStyles, Theme } from '@material-ui/core';
-
-export const styles = ({}: Theme) =>
-  createStyles({});
-`
-};
-
-module.exports.getStylesTemplate = getStylesTemplate;
